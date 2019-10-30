44d01c544718008065e23a1841dd68e467d28ba1
Jeffrey Nguyen <jeffmiguel7@gmail.com>
2019-10-30 02:48:01 -0700

    redid structure

--- src/components/ShopHome/index.js
+++ src/components/ShopHome/index.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { withFirebase } from '../Firebase';
-class ShopHome extends Component{
-    render(){
-        return <h1>shop home</h1>;
-    }
+import { compose } from "recompose";
+
+import { withAuthorization, withEmailVerification } from "../Session";
+
+import { withFirebase } from "../Firebase";
+class ShopHome extends Component {
+  render() {
+    return <h1>shop home</h1>;
+  }
 }
-export default ShopHome;
+const condition = authUser => !!authUser;
+export default compose(
+  withEmailVerification,
+  withAuthorization(condition)
+)(ShopHome);
