f433ff35d97e0fb82b240c7204560e4d93ac89ed
josephzhaoballer <josephzhao15@gmail.com>
2019-11-11 14:31:06 -0800

    fixed file name

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { compose } from "recompose";
-import "./SignupStyle.css";
+import "./SignUpStyle.css";
 import mainLogo from "../../assets/logo.png";
 
 import { withFirebase } from "../Firebase";
